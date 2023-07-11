dcda103006e72973dff4310ee8d6b0acd0f1e89e
James <136427245+JxmesTheDuck@users.noreply.github.com>
2023-07-11 08:12:01 +0200

    Update STAUBSAUGER.java
    ok

--- EigenesProjekt/Staubsauger_Vorlage/STAUBSAUGER.java
+++ EigenesProjekt/Staubsauger_Vorlage/STAUBSAUGER.java
@@ -9,13 +9,43 @@ public class STAUBSAUGER extends BILD
 {
     // Attribute
     
-
+    private double geschwindigkeit;
+    private double deltaX;
+    private double deltaY;
+    private int winkele=nenneWinkel();
     // Konstruktor
+
+    public STAUBSAUGER(int x,int y)
+    {
+        super(x,y,"robot.gif");
+        this.setzeMittelpunkt(x, y);
+        geschwindigkeit=10;
+        deltaX=10;
+    }
     public STAUBSAUGER()
     {
-        super(ZIMMER.gibAnzeigeBreite(),ZIMMER.gibAnzeigeHoehe(),"Robot.gif");
+        this(400,300);
+    }
+    
+    // Methoden
+    void berechneNeuSchrittweite()
+    {
+        deltaX=geschwindigkeit*cos_Drehwinkel();
+        deltaY=-geschwindigkeit*sin_Drehwinkel();
     }
 
-    // Methoden
+    @Override
+    public void setzeDrehwinkel(int winkel){
+        super.setzeDrehwinkel(winkel);
+        this.berechneNeuSchrittweite();
+    }
     
+
+    public void bewegen(){
+        verschiebenUm(deltaX, deltaY);
+    }
+    public void setzeGeschwindigkeit(double vNeu){
+        geschwindigkeit=vNeu;
+    }
 }
+
